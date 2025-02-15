55836dcbfd735d84786ca33785a27c1ab6fdcb4d
Thunder-Flag <harshbhatia0007@gmail.com>
2025-02-15 20:37:48 +0530

    initial commit

---
+++ php_codes/code33.php
@@ -0,0 +1,12 @@
+<?php
+
+    	function writeMessage()
+    	{
+        	echo "Hello! Manshay this side.";
+    	}
+    
+    	writeMessage();
+	
+	echo "<br><br>";
+    echo "Code executed by Harsh Bhatia(0221BCA121)";
+?>
